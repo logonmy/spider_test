2a3be8c25edb0baa2610764c7ae19f61f8bbd827
cqcpcqp <cqcpcqp@163.com>
2018-03-25 16:25:01 +0800

    好奇心日报首页爬虫通过

--- 爬虫平台Spider/_haoqixin/business/script.js
+++ 爬虫平台Spider/_haoqixin/business/script.js
@@ -9,7 +9,7 @@ try{
     for(let  i = 0;i< document.querySelectorAll(".slick-track a").length;i++){
         let str = "https://www.qdaily.com/" +document.querySelectorAll(".slick-track a")[i].getAttribute("href")
         if(str.indexOf("article") > -1){
-            templateData.items.push(str);
+            templateData.items.push({url:str});
         }
         datas.push(str);
     }
@@ -24,19 +24,19 @@ try{
     for(var item of items){
         let str = "http://www.qdaily.com" + item.querySelector("a").getAttribute("href");
         if(str.indexOf("article") > -1){
-            templateData.items.push(str);
+            templateData.items.push({url:str});
         }
     }
 
     for(let data of datas){
         if(data != null){
-            templateData.items.push(data);
+            templateData.items.push({url:data});
         }
     }
     console.log(templateData);
 
-    //chrome.runtime.sendMessage(templateData);
-    //window.close()
+    chrome.runtime.sendMessage(templateData);
+    window.close()
 
 }catch(e){
     chrome.runtime.sendMessage({
