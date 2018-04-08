79d6f40ccb1b815f7ca36df4fed92a30df6d9fa2
俞晨光 <yuchenguang@wantdata.cn>
2018-04-08 17:58:04 +0800

    update duowanPicDetail

--- HiveSpider/_duowanPicDetail/background.js
+++ HiveSpider/_duowanPicDetail/background.js
@@ -15,19 +15,29 @@ require([
 
     const DETAIL_BEE_NAME = "duowanPic_index_detail";
 
+    const filterItem = async(task) => {
+        let query = {
+            partition: DETAIL_BEE_NAME,
+            keys: [task.value]
+        };
+        let res = await Http.call(`http://bee.api.talkmoment.com/dereplicate/filter/by/history`, query);
+        return res.filter_result[0];
+    };
+
     const postDataToMessage = async(task, data) => {
         await Http.call(`http://bee.api.talkmoment.com/message/publish?topic=${DETAIL_BEE_NAME}`, data);
     };
 
-    const postWashTask = async(detailTask) => {
+    const postWashTask = async(detailTask, data) => {
         let washTask = {
             name: "wash_corpus",
             value: "",
             config: JSON.stringify({
                 bee_source: DETAIL_BEE_NAME,
-                msg_topic: "wash_" + DETAIL_BEE_NAME,
-                brick_id: JSON.parse(detailTask.config).brick_id
+                brick_id: JSON.parse(detailTask.config).brick_id,
+                publish: true
             }),
+            data: JSON.stringify(data),
             scheduled_at: Date.now()
         };
         await Http.call("http://bee.api.talkmoment.com/scheduler/task/post", washTask)
@@ -50,7 +60,7 @@ require([
                     }
                 }
                 return false;
-            }
+            };
             let result = [];
 
             for(let a of arr){
@@ -64,51 +74,57 @@ require([
         try {
             Socket.log(`开始处理爬取任务,task=`, task);
 
-            Socket.log(`打开网页Tab(url=${task.value}), 注入爬取逻辑`);
-            let baseUrl = task.value;
+            let filter = await filterItem(task);
+            if (!filter) {
+                Socket.log(`网页(url=${task.value})已经爬取过, 跳过`);
+            } else {
+                Socket.log(`打开网页Tab(url=${task.value}), 注入爬取逻辑`);
+                let baseUrl = task.value;
 
-            let tab0 = new Tab(task.value, ["./business/script1.js"]);
-            let pageCount = await tab0.run();
-            Socket.log("当前图库有"+ pageCount + "张图片");
+                let tab0 = new Tab(task.value, ["./business/script1.js"]);
+                let pageCount = await tab0.run();
+                Socket.log("当前图库有"+ pageCount + "张图片");
 
-            let dataArray = [];
-            for(let i=1;i <= pageCount;i++){
-                let pageUrl = task.value;
-                pageUrl = pageUrl + "#p" + i;
-                let tab = new Tab(pageUrl, ["./business/script.js"]);
+                let dataArray = [];
+                for(let i = 1;i <= pageCount; i++){
+                    let pageUrl = task.value;
+                    pageUrl = pageUrl + "#p" + i;
+                    let tab = new Tab(pageUrl, ["./business/script.js"]);
 
-                Socket.log(`开始爬取`);
-                let data = await tab.run();
-                Socket.log(`爬取完成,data=`, data);
-                data.comments = arrayDulicate(data.comments);
-                dataArray.push(data);
+                    Socket.log(`开始爬取`);
+                    let data = await tab.run();
+                    Socket.log(`爬取完成,data=`, data);
+                    data.comments = arrayDulicate(data.comments);
+                    dataArray.push(data);
 
-                task.data = JSON.stringify(data);
-                Socket.log(`发送爬取结果到消息队列topic=${task.name}`);
-                await postDataToMessage(task, data);
-                //FileControll.append("_duowanPicDetail", JSON.stringify(data) + "\n");
-                Socket.log(`发起清洗任务`);
-                await postWashTask(task);
+                    task.data = JSON.stringify(data);
+                    Socket.log(`发送爬取结果到消息队列topic=${task.name}`);
+                    await postDataToMessage(task, data);
+                    Socket.log(`发送爬取结果到消息队列完成`);
+
+                    Socket.log(`发起清洗任务`);
+                    await postWashTask(task, data);
+                }
+
+                Socket.log(`添加内容url(${data.url})到去重模块的历史集合`);
+                await postDataToDereplicate(task, {url: baseUrl});
+                Socket.log(`添加到去重模块成功`);
+
+                task.data = JSON.stringify(dataArray);
+                Socket.log(`提交爬取任务结果数据`);
+                await Task.putTaskData(task);
+                Socket.log(`提交爬取任务结果数据完成`);
             }
-
-            Socket.log(`添加内容url(${data.url})到去重模块的历史集合`);
-            await postDataToDereplicate(task, {url: baseUrl});
-            Socket.log(`添加到去重模块成功`);
-
-            task.data = JSON.stringify(dataArray);
-            Socket.log(`提交爬取任务结果数据`);
-            await Task.putTaskData(task);
-            Socket.log(`提交爬取任务结果数据完成`);
-
-            Socket.log(`上报爬取任务成功,task=`, task);
-            await Task.resolveTask(task);
-            Socket.log(`爬取任务完成`);
 
             Socket.emitEvent({
                 event: "detail_item_finish",
                 bee_name: task.name,
                 bee_id: task.id
             });
+
+            Socket.log(`上报爬取任务成功,task=`, task);
+            await Task.resolveTask(task);
+            Socket.log(`爬取任务完成`);
         } catch(err) {
             Socket.error("爬取失败,err=", err.stack);
             Socket.log(`上报爬取任务失败,task=`, task);
